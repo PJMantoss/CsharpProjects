7ddf2a9b6e605610f92ca77d88eef174e9116135
PJ Mantoss <joelptoss@gmail.com>
2024-05-22 12:01:24 +0100

    block comment exercise

--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31d3175071a5acea1f0c0e438079d582f191947d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ac523caa4d0373f2b4bcae10e8d948f43649d52")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
