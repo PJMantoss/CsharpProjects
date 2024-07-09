40d6081bb1cb3e71aa1cf8ee8516219cde13bc5a
PJ Mantoss <joelptoss@gmail.com>
2024-07-09 19:29:43 +0100

    Scope and Boolean Expressions

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -30,6 +30,72 @@ convert it back and print it out.*/
 // Console.WriteLine(!pangram.Contains("fox"));
 // Console.WriteLine(!pangram.Contains("cow"));
 
-int saleAmount = 1001;
+// int saleAmount = 1001;
 // int discount = saleAmount > 1000 ? 100 : 50;
-Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
+// Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
+
+// Random coin = new Random();
+// int flip = coin.Next(0, 2);
+
+// Console.WriteLine((flip == 0) ? "Heads" : "Tails");
+
+// string permission = "Admin|Manager";
+// int level = 53;
+
+// if(permission.Contains("Admin"))
+// {
+//     if(level > 55)
+//     {
+//         Console.WriteLine("Welcome, Super Admin user.");
+//     }
+//     else 
+//     {
+//         Console.WriteLine("Welcome, Admin user.");
+//     }
+// }
+// else if(permission.Contains("Manager"))
+// {
+//     if (level >= 20)
+//     {
+//         Console.WriteLine("Contact an Admin for access.");
+//     }
+//     else 
+//     {
+//         Console.WriteLine("You do not have sufficient privileges.");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("You do not have sufficient privileges.");
+// }
+
+// string name = "steve";
+// if (name == "bob") Console.WriteLine("Found Bob");
+// else if (name == "steve") Console.WriteLine("Found Steve");
+// else Console.WriteLine("Found Chuck");
+
+// int[] numbers = { 4, 8, 15, 16, 23, 42 };
+
+// int total = 0;
+// bool found = false;
+
+// foreach (int number in numbers)
+// {
+//     total += number;
+
+//     if (number == 42) found = true;
+    
+// }
+
+// if (found) Console.WriteLine("Set contains 42");
+
+// Console.WriteLine($"Total: {total}");
+
+int first = 1;
+if(first > 0)
+{
+    int second = 8;
+    first += second;
+}
+
+Console.WriteLine(first);
