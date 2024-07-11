84db745d93776b41f6292a5ab3a034ec012b9e68
PJ Mantoss <joelptoss@gmail.com>
2024-07-11 12:11:14 +0100

    do-while practice

--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0508daef7adf2feb763228837a0217ecdd987ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da7940187590948c72f3298ad572466bca8a5a1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
